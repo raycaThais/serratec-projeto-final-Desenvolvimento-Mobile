b67672a11f371baf345136d063e8af81435a6a90
unknown <carlosotoline@gmail.com>
2025-06-25 21:26:13 -0300

    iniciando

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,20 +1,25 @@
-import { TextInput, TouchableOpacity, View, Text } from "react-native"
+import { TouchableOpacity, View, Text } from "react-native"
 import React from "react"
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
+import { styles } from "./styles";
 
 export const Login = () => {
 
     return(
-        <View>
-            <Input />
-            <Input />
-            <Button nome={"Logar"} />
-            <TouchableOpacity>
-                <Text>
-                    Cadastre-se
-                </Text>
-            </TouchableOpacity>  
+        <View style={styles.container}>
+            
+            <View style={styles.login}>
+                <Text style={styles.titulo}>Login</Text>
+                <Input  placeholder="email"/>
+                <Input  placeholder="password"/>
+                <Button nome={"Logar"} />
+                <TouchableOpacity>
+                    <Text>
+                        Cadastre-se
+                    </Text>
+                </TouchableOpacity> 
+            </View>
         </View>
     )
 }
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+container:{
+    flex: 1,
+},
+
+login:{
+    backgroundColor: '#fff',
+    
+},
+
+titulo:{
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginVertical: 90,
+    textAlign: 'center',
+    marginBottom: 20
+},
+
+})
