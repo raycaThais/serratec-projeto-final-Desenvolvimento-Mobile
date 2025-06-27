398b37b7ab07ac7b40fa01ff3ad9318cc26d9dc5
unknown <carlosotoline@gmail.com>
2025-06-27 20:32:21 -0300

    oi

---
+++ src/screens/Cadastro/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+
+    container:{
+        flex: 1
+    },
+
+    cadastro:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    title:{
+        color: '#000',
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginVertical: 90,
+        textAlign: 'center',
+        marginBottom: 20
+    },
+
+    texto:{
+        fontWeight: 'bold',
+        textDecorationLine: 'underline',
+    }
+
+})
