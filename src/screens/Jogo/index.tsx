05aa8505678a93d0532fe08459bc94334d39ad55
Rayca Thais <raycathaisbn@gmail.com>
2025-06-26 22:18:08 -0300

    Continuando a lógica do jogo

--- src/screens/Jogo/index.tsx
+++ src/screens/Jogo/index.tsx
@@ -9,6 +9,8 @@ export const Jogo = () => {
     const [vezJogador, setVezJogador] = useState<boolean>(true)
     const [cartaAtualJogador, setCartaAtualJogador] = useState<OnePieceCharacter>()
     const [cartaAtualBot, setCartaAtualBot] = useState<OnePieceCharacter>()
+    const [cartasJogador, setCartasJogador] = useState<OnePieceCharacter[]>([])
+    const [cartasBot, setCartasBot] = useState<OnePieceCharacter[]>([])
 
     const embaralharCartas = () => {
         const baralhoEmbaralhado = [...baralho].sort(() => Math.random() - 0.5)
@@ -17,9 +19,9 @@ export const Jogo = () => {
 
     const dividirCartas = (baralho: OnePieceCharacter[]) => {
         const cartasJogador = baralho.slice(0, 5)
+        setCartasJogador(cartasJogador)
         const cartasBot = baralho.slice(5, 10)
-
-        return { cartasJogador, cartasBot }
+        setCartasBot(cartasBot)
     }
 
     const pegarPrimeiraCartaJogador = (baralhoJogador: OnePieceCharacter[]) => {
@@ -58,8 +60,38 @@ export const Jogo = () => {
         return melhorAtributo
     }
 
-    //falta o método do jogador escolher o atributo da carta
-    //falta o método de comparação dos atributos escolhidos e indicar o vencedor da rodada
+    const compararAtributos = (atributo: keyof OnePieceCharacter) => {
+
+       const atributoBot =  cartaAtualBot? cartaAtualBot[atributo]! : 0;
+       const atributoJogador = cartaAtualJogador? cartaAtualJogador[atributo]!: 0 
+
+       if (atributoBot > atributoJogador) {
+
+        //bot vence
+        // cartas vao para o baralho do bot
+       } else if (atributoJogador > atributoBot){
+        //jogador vence
+        // cartas vao para o baralho do jogador
+      
+       }else{
+        // empate (tem que repetir a jogada) -> mesma pessoa que escolheu o atributo, escolhe dnv
+       }
+
+    }
+
+    const escolherAtributoJogador = (atributo : keyof OnePieceCharacter) => {
+        compararAtributos(atributo)
+    }
+
+    const realizarJogadaBot = () => {
+       const atributoEscolhidoBot = escolherMelhorAtributo(cartaAtualBot!) as keyof OnePieceCharacter
+
+       compararAtributos(atributoEscolhidoBot)
+
+    }
+
+
+    //falta o método de comparação dos atributos escolhidos e indicar o vencedor da rodada(completar)
     //falta o método de colocar as cartas usadas no baralho do vencedor
     //falta fazer useEffect para o jogada do bot
     //falta todo o fluxo da partida (que chama os métodos feitos)
