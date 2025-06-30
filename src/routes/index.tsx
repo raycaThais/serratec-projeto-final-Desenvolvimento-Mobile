162a3d588801d9fe09c35a2b725ccccdaafdef59
Lívia Raissinger <liviaraissinger@gmail.com>
2025-06-30 14:46:25 -0300

    instalei a nav

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { NavigationContainer } from '@react-navigation/native';
+
+export const Routes = () => {
+    return (
+        <NavigationContainer>
+
+        </NavigationContainer>
+    );
+}
